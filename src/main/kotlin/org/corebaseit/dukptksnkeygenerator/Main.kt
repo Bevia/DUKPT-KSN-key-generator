3094b088507eb34c4d6062e7b450634af583e8c4
vbevia <vbevia@gmail.com>
2025-06-20 11:22:04 +0200

    clean code

--- src/main/kotlin/org/corebaseit/dukptksnkeygenerator/Main.kt
+++ src/main/kotlin/org/corebaseit/dukptksnkeygenerator/Main.kt
@@ -4,7 +4,7 @@ fun main() {
     val args = arrayOf(
         "-k1", "0123456789ABCDEF0123456789ABCDEF", // First partial key
         "-k2", "FEDCBA9876543210FEDCBA9876543210", // Second partial key
-        "-k3", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA00", // Third partial key
+        "-k3", "FEDCBAAA76543210FEDCBBC876543210", // Third partial key
         "-k", "0123456789ABCDEF0123"               // KSN value
     )
     DukptCliTool(args).run()
